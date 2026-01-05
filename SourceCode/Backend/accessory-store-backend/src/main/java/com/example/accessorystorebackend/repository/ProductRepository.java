706e670ccc0f7beafbbb51d7139fce67720ceeb8
Yusuf <yusufardasn@gmail.com>
2026-01-05 19:32:23 +0300

    Update

--- SourceCode/Backend/accessory-store-backend/src/main/java/com/example/accessorystorebackend/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.accessorystorebackend.repository;
-
-import com.example.accessorystorebackend.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
